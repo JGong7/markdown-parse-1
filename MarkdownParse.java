0e7773e24bcf5c1efed2b53a3011a20a13644d98
CATHCHEN014 <97484142+CATHCHEN014@users.noreply.github.com>
2022-01-20 14:39:15 -0800

    Update MarkdownParse.java
    we make the change in the while loop so it doesn't print the image link

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,15 +13,17 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.println(currentIndex);
+            //System.out.println(currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             //the second argument is for where to start searching
             //if not provided, then it will start seraching at index 0
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
+            if (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!'){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
+            }
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+            //System.out.println(currentIndex);
         }
         return toReturn;
     }
