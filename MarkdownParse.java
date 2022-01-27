42d3323b34781538d7451624adcb390b34ede290
CATHCHEN014 <97484142+CATHCHEN014@users.noreply.github.com>
2022-01-27 13:50:42 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -17,7 +17,7 @@ public class MarkdownParse {
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             //toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!'){
+            if (nextCloseBracket == openParen - 1 && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
