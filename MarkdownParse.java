70fdb6e5cd1d190941a06ae259d0cfb649af471c
CATHCHEN014 <97484142+CATHCHEN014@users.noreply.github.com>
2022-02-03 13:35:45 -0800

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,19 +12,16 @@ public class MarkdownParse {
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
-            System.out.println(currentIndex);
+            //System.out.println(currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);//the second argument is for where to start research, if not provided, search start from index 0
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
             //toReturn.add(markdown.substring(openParen + 1, closeParen));
-            if (markdown.substring(openParen + 1, closeParen).contains(".")){
-                if ((markdown.substring(openParen+1, closeParen).contains(".")) && (nextCloseBracket == openParen - 1) 
-                && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!')){
-                    toReturn.add(markdown.substring(openParen + 1, closeParen));
-                }
+            if (nextCloseBracket + 1 == openParen && closeParen-openParen != 1 && (nextOpenBracket == 0 || markdown.charAt(nextOpenBracket - 1) != '!' )){
+                toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;
-            System.out.println(currentIndex);
+            //System.out.println(currentIndex);
         }
         return toReturn;
     }
